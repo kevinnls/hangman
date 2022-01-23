6b205743140d61b563d249e48c3aebbbd149b431
kevinnls <57634663+kevinnls@users.noreply.github.com>
2022-01-23 18:47:03 +0100

    more basics

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header>
+	<h1>Hangman</h1>
+</header>
+
+<main />
+
+<footer>
+	&copy; Kevin Samuel <a href="mailto:developer+hangman@kevinnlsamuel.com"
+		>&lt;developer@kevinnlsamuel.com&gt;</a
+	>
+</footer>
+
+<style>
+	footer {
+		text-align: center;
+		width: 100%;
+		background-color: var(--theme-dark);
+		filter: opacity(30%);
+		padding: 0.8em 0;
+	}
+	footer > a {
+		color: var(--fg-color);
+	}
+	footer:focus,
+	footer:hover {
+		filter: opacity(100%);
+	}
+</style>
