02e5dd587e376b417893fc0b46a59deb0fe3609c
kevinnls <57634663+kevinnls@users.noreply.github.com>
2022-01-23 23:25:04 +0100

    create App component

---
+++ src/components/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let db;
+	let deck = db?.deck;
+
+	let randomIndex: number = Math.floor(Math.random() * deck.length);
+	let currentWord = deck[randomIndex].word.toLowerCase();
+	let guessedLetters = [];
+
+	$: regex = new RegExp(`[^${guessedLetters.join('')}]`, 'ig');
+	$: wordInProgress = currentWord.replaceAll(regex, '_');
+
+	let lostLivesCount = 0;
+</script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import Footer from '../components/Footer.svelte';
+	import App from '../components/App.svelte';
+	import * as sampleDb from '../const/sampleDb.json';
 </script>
 
 <header>
 	<h1>Hangman</h1>
 </header>
 
-<main />
+<main>
+	<App db={sampleDb} />
+</main>
 
 <Footer />
