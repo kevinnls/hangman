d001e84988e8bc86ea5f04e10e9fbe0c584dabc9
kevinnls <57634663+kevinnls@users.noreply.github.com>
2022-01-28 16:10:31 +0530

    patch: fix svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({ fallback: '404.html' }),
-		paths: dev ? '' : '/hangman',
+		paths: { base: dev ? '' : '/hangman' },
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
