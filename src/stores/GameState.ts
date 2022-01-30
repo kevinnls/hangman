7056adc5c7d63847f47688342e188de0077096f6
kevinnls <57634663+kevinnls@users.noreply.github.com>
2022-01-30 21:18:27 +0100

    make GameState persist to localStorage on change

--- src/stores/GameState.ts
+++ src/stores/GameState.ts
@@ -3,10 +3,19 @@ import { writable } from 'svelte/store';
 import { defaultGameState } from 'src/models/GameState';
 class GameState {
 	constructor(defaultState = defaultGameState) {
-		if (browser)
-			JSON.parse(localStorage.getItem('gamestate'))(
-				({ subscribe: this.subscribe, update: this.update, set: this.set } = writable(defaultState))
-			);
+		let initialState = { ...defaultGameState, ...defaultState };
+		let localState = null;
+		if (browser) {
+			localState = JSON.parse(localStorage.getItem('gamestate'));
+			if (localState)
+				//TODO: check if it's actually GameStateModel
+				initialState = { ...localState };
+		}
+		({ subscribe: this.subscribe, update: this.update, set: this.set } = writable(defaultState));
+
+		this.subscribe((currentState) => { //write all changes to game state to localStorage
+			if (browser) localStorage.setItem('gamestate', currentState);
+		});
 	}
 	subscribe;
 	update;
